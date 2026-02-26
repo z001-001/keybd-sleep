1b0e32eda918fdd15cffe266d3525b7c1d9cec28
ISHIKAWA Hirokazu <z001-001@drmscp.net>
2026-02-26 09:46:36 +0900

    Write copyright and authors.

--- build.rs
+++ build.rs
@@ -9,7 +9,7 @@ fn main() {
         // メタデータの設定
         res.set("ProductName", "KeybdSleep");
         res.set("CompanyName", "");
-        res.set("LegalCopyright", "Copyright © 2026");
+        res.set("LegalCopyright", "Copyright © 2026 ISHIKAWA Hirokazu");
         res.set("FileDescription", "KeybdSleep");
         
         // 実行ファイル名を個別に指定する場合（前述の [[bin]] と合わせる）
